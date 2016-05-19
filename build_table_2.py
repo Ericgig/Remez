3a469b161b6f4f1dbe89a71a740a7de8bb8f96d0
Ericgig <eric.giguere@calculquebec.ca>
2016-05-19 12:26:38 -0400

    v1+start v2

--- build_table_2.py
+++ build_table_2.py
@@ -1,7 +1,7 @@
 #Remez make table based on least square
 #
 # For x^p, -1<p<1, x_min<=x<=x_max find
-#   a0,a[i],b[i] (i=1:k) so that a0+sum(a[i]/(x+b[i])) ~ x^p
+#   a0,d[i],b[i] (i=1:k) so that a0+sum(d[i]/(1+x/b[i])) ~ x^p
 #   within err
 #
 # Input
@@ -11,15 +11,14 @@
 #   k   :  number of term of the approximation           ,   >= 0
 #
 # Output
-#   a0, a[i], b[i] : terms of the approximation
+#   a0, d[i], b[i] : terms of the approximation
 #   err            : maximum error of the best fit
 #
-# 1) scipy least-square
-#     => Can't get double precision on fit, is sattisfied with the convergence too easily.
-#      Want a maximum absolute error of at most 10e-15 on any point in the range
-#      
-# try 2) less points to fit but stricter condition
-#     => Not enough control with least_square, converge before condition
+# 3) Will use my own least square algoritme
+#    Will be slower => Numpy optimization first, then numba or cython 
+#    
+#    Change the coefficient : d = a/b 
+# 
 
 
 import numpy as np
@@ -32,25 +31,55 @@ type = np.float64
 
 #make the function
 def make_fx_lin(x_min,x_max,p,N):
-    #X = x_min+np.arange(N,dtype=type)*((x_max-x_min)/(N-1))
     X = np.linspace(x_min, x_max, N, endpoint=True, dtype=type)
     Y = X**p
     return X,Y
     
 def make_fx_log(x_min,x_max,p,N):
-    #step = np.exp(np.log(x_max/x_min)/(N-1))
-    #X = x_min*step**np.arange(N,dtype=type)
     X = np.logspace(x_min, x_max, N, endpoint=True, dtype=type)
+    Y = X**p
+    return X,Y
+    
+def make_fx_rand(x_min,x_max,p,N):
+    X = np.random.rand(N, dtype=type)*(x_max-x_min)+x_min
+    X = np.sort(X)
     Y = X**p
     return X,Y
 
 def make_fx(x_min,x_max,p,N):
-    x1,y1 = make_fx_log(x_min,x_max,p,N/2)
-    x2,y2 = make_fx_log(x_min,x_max,p,N/2)
-    X = np.concatenate((x1,x2))
-    Y = np.concatenate((y1,y2))
+    X, Y  = make_fx_log(x_min,x_max,p,int(N/3))
+    x1,y1 = make_fx_log(x_min,x_max,p,int(N/3))
+    X = np.concatenate((X,x1))
+    Y = np.concatenate((Y,y1))
+    x1,y1 = make_fx_log(x_min,x_max,p,int(N/3))
+    X = np.concatenate((X,x1))
+    Y = np.concatenate((Y,y1))
+    i = np.argsort(X)
+    X = X[i];Y=Y[i]
     return X,Y
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 def f_approx(x, coef):  #x vector no scalar
     k = (len(coef)-1)/2
@@ -74,13 +103,28 @@ def D_f_app(coef,x,y):
         derr.append(d)
     return derr
 
+    
 def sort_coef(coef):
-    k = int((len(coef)-1)/2)
-    a = coef[1:k+1]
-    b = coef[k+1:]
-    ind = np.argsort(b)
-    coef[1:k+1] = a[ind]
-    coef[ k+1:] = b[ind]
+    ind = np.argsort(coef[2])
+    coef[2] = coef[2][ind]
+    coef[1] = coef[1][ind]
+    
+def def_coef(k,p):
+    a0 = 1
+    d = -p*np.arange(k,dtype=type)/k  # the sign of the d are usualy opposed to the sign of p
+    b = np.arange(k,dtype=type)       # positive so the there is no singularity in the range
+    return [a0,d,b]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 def run_min(x_min,x_max,p,N,coef):
     X,Y = make_fx(x_min,x_max,p,N)
@@ -94,9 +138,7 @@ def run_min(x_min,x_max,p,N,coef):
        
 def main(p, k, x_min, x_max):
     #set initial guess
-    coef = np.zeros(2*k+1,dtype=type)
-    coef[0:k+1] = np.arange(k+1, dtype=type)+.5
-    coef[k+1:]  = np.arange(k, dtype=type)
+
 
     f = 3*k
     tries = 0
